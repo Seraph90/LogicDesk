200794166eadf49e2edaea7ffc4ef35358f97ed6
Heifetz <Seraph_90@mail.ru>
2015-12-27 21:58:23 +0300

    Refactor

--- Unit1.pas
+++ Unit1.pas
@@ -10,38 +10,41 @@ uses
 type
 	TForm1 = class(TForm)
 
-		// Рабочее поле
+		// Work field
 		I_Table: TImage;
 
-		// Поле выбора
+		// Change field
 		I_El: TImage;
 		procedure FormCreate(Sender: TObject);
 		procedure I_TableMouseUp(Sender: TObject; Button: TMouseButton;
 			Shift: TShiftState; X, Y: Integer);
 		procedure FormClose(Sender: TObject; var Action: TCloseAction);
 
-		// Прорисовка провода
 		procedure RedrawWire;
 
-		// Проверка питания и вызов проверки логики
+		// Connect test and logic test
 		procedure Test;
 
-		// Расчёт логики
+		// Calculate logic
 		function TestLink(FPin: TPin): Boolean;
 
-		// Перерисовка поля
+		// Work field redraw
 		procedure Redraw;
 
-		// Выбор элемента в поле выбора
+		// Change element on work field
 		procedure Turn(Sender: TObject);
 	private
 	public
 	end;
 
 const
-	panelCol = 6;
+  // The number of selectable items
   elCount = 6;
+
+  // Battaries conteiner type
   batType = 1;
+
+  // Signal generators lamp conteiner type
   sendLampType = 3;
 
 var
@@ -49,10 +52,10 @@ var
 	
 	Cur: Integer;
 
-	// Рабочее поле
+	// Work field
 	Field: array[0..6, 0..7] of TCont;
 
-	// Первый выыбранный контакт
+	// First selected pin
 	FstPin: TPin;
 
 implementation
@@ -60,34 +63,36 @@ implementation
 {$R *.dfm}
 {$R WindowsXP.res}
 
-// Расчёт логики
+// Calculate logic
 function TForm1.TestLink(FPin: TPin):Boolean;
 var
 	res: Boolean;
 begin
 	TestLink := False;
-  //res := False;
+  res := False;
 	if (FPin.Link[1] = nil) then
 		case FPin.PCont.CType
 		of
-			// Элементы у которых нет логики
+
+			// Elements without logic
 			1..4: Exit;
-			// Элементы у которых на свободных контактах логическая 1
+
+			// Elements with logic 1 on free pin
 			5..5: begin
 							TestLink := True;
 							FPin.State := True;
 						end;
 
-			// Элементы у которых на свободных контактах логический 0
+			// Elements with logic 0 on free pin
 			6..7: begin
 							TestLink := False;
 							FPin.State := False;
 						end;
-      {<}
+
 		end
 	else
 		begin
-			// Проверка на рекурсию
+			// Check for recursion
 			if FPin.Link[1].Rec
 			then
 				begin
@@ -99,10 +104,10 @@ begin
 					FPin.Link[1].Rec := True;
 				end;
 
-			// Описание логики
+			// Describing logic
 			case (FPin.Link[1].PCont.CType) of
 
-        // Лампочка
+        // Lamp
 				3: res := FPin.Link[1].PCont.CEl[FPin.Link[1].PEl].ElOut.State;
 
         // not And
@@ -130,7 +135,7 @@ begin
 		end;
 end;
 
-// Проверка питания и вызов проверки логики
+// Connect test and logic test
 procedure TForm1.Test;
 var
 	i, j, k, ii, jj: Integer;
@@ -174,7 +179,6 @@ begin
 			ta[i].CEl[j].ElIn[1].State := TestLink(ta[i].CEl[j].ElIn[1]);
 end;
 
-// Прорисовка провода
 procedure TForm1.RedrawWire;
 var
 	i, j, k, z: Integer;
@@ -197,7 +201,7 @@ begin
 						end;
 end;
 
-// Перерисовка поля
+// Work field redraw
 procedure TForm1.Redraw;
 var
 	i, j: Integer;
@@ -213,7 +217,7 @@ begin
 			end;
 end;
 
-// Выбор элемента в поле выбора
+// Change element on work field
 procedure TForm1.Turn(Sender: TObject);
 var
 	i: Integer;
@@ -224,7 +228,7 @@ begin
 				begin
 					Field[0, i].Redraw(clSilver, False);
 					Cur := i + 1;
-					{>
+					{> If elements not organized
 					case i
 					of
 						1, 2: Cur := i;
@@ -244,13 +248,14 @@ procedure TForm1.FormCreate(Sender: TObject);
 var
 	i, j: Integer;
 begin
-	// Очистка поля
+	// Clearing field
 	for i := 1 to 6 do
 		for j := 1 to 7 do
       begin
   			Field[i, j] := nil;
       end;
-	// Создание элементов на поле
+
+	// Creates elements on work field
 	Field[1, 1] := TCont.Create(I_Table, 0, 0, 1, True);
 	Field[1, 1].OnLine := True;
 	Field[1, 2] := TCont.Create(I_Table, 0, 1, 3, True);
@@ -259,7 +264,7 @@ begin
 	Field[2, 2].OnLine := True;
 	Field[3, 2] := TCont.Create(I_Table, 2, 1, 4, True);
 
-	// Отрисовка элементов на поле выбора
+	// Creates elements on change field
 	Field[0, 1] := TCont.Create(I_El, 0, 0, 2, False);
 	Field[0, 2] := TCont.Create(I_El, 0, 1, 3, False);
 	Field[0, 3] := TCont.Create(I_El, 0, 2, 4, False);
@@ -276,7 +281,7 @@ begin
 	Redraw;
 end;
 
-// Создание нового элемента на поле при отпускании кнопки мыши
+// Create new element on mouse up
 procedure TForm1.I_TableMouseUp(Sender: TObject; Button: TMouseButton;
 	Shift: TShiftState; X, Y: Integer);
 var
@@ -296,7 +301,7 @@ procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
 var
 	i, j: Integer;
 begin
-	// Уничтожение элементов на поле
+	// Destroying all elements 
 	for i := 1 to 6 do
 		for j := 1 to 7 do
 			if (Field[i, j] <> nil) then
